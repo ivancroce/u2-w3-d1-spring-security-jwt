ed514475a253e8a4e2a329a6ee0940e080d1bcc0
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-07-28 11:59:44 +0100

    Initial commit

---
+++ src/main/java/ictech/u2_w3_d1_spring_security_jwt/U2W3D1SpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package ictech.u2_w3_d1_spring_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W3D1SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W3D1SpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ictech/u2_w3_d1_spring_security_jwt/U2W3D1SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ictech.u2_w3_d1_spring_security_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W3D1SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
